a077824b70660baf42d8d380a438b99b259e27f0
Alex <carlos.mejia1342@gmail.com>
2019-01-15 12:48:31 -0800

    Fixed TimeUnits function bug (not formatting seconds correctly)

--- src/lib/TimeUnits.js
+++ src/lib/TimeUnits.js
@@ -1,17 +1,13 @@
 export default class TimeUnits{
-    /*constructor(seconds){
-        this.state={
-            seconds : 0
-        }
-    }*/
 
     static secondsToClock(seconds){
-        var minutes = Math.floor(seconds / 60)
-        var secs = Math.floor(seconds -  minutes * 60)
         var hours = Math.floor(seconds / 3600)
+        var minutes = Math.floor((seconds - (hours * 3600)) / 60)
+        var secs = Math.floor(seconds - (hours * 3600) - (minutes * 60))
 
         return `${10 > hours ? '0' + hours : hours} : 
         ${10 > minutes ? '0' + minutes : minutes} : 
-        ${10 > secs ? '0' + seconds : seconds}`
+        ${10 > secs ? '0' + secs : secs}`
     }
+    
 }
